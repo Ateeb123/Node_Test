9bee82688d71d2baeb3df5534339a8afc1102a4f
Ateeb Ahmed <aa02751@st.habib.edu.pk>
2019-07-24 16:21:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -90,6 +90,25 @@ app.post('/webhook', (req, res) => {
 
     console.log('CHECK');
     console.log(req.body);
+      
+        
+    console.log('CHECK');
+    console.log('CHECK');
+    console.log('CHECK');
+
+    console.log('CHECK');
+      
+    body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
+
+    // Get the sender PSID
+    let sender_psid = webhook_event.sender.id;
+    console.log('Sender PSID: ' + sender_psid);
+
+});
   });
 
 
