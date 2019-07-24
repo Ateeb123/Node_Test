b0fa7fbd936f61bfdd4c66d6b8eb832d96b3d169
Ateeb Ahmed <aa02751@st.habib.edu.pk>
2019-07-24 16:34:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -98,17 +98,59 @@ app.post('/webhook', (req, res) => {
 
     console.log('CHECK');
       
-    bodyy.entry.forEach(function(entry) {
+   let responseJson = {};
 
-      // Gets the body of the webhook event
-    let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
+    switch (action) {
+        case "buy.drone-types":
+            responseJson.fulfillmentText = 'Available drones here from fulfillment';
+            let richResponses = [
+                {
+                    "text": {
+                        "text": [
+                            "Here is a list of all available Beginners Drone"
+                        ]
+                    },
+                    "platform": "FACEBOOK"
+                },
+                {
+                    "card": {
+                        "title": "ALTAIR AERIAL AA108",
+                        "subtitle": "FOR YOUNG DRONE ENTHUSIASTS & BEGINNERS",
+                        "imageUri": "https://mydeardrone.com/wp-content/uploads/2018/11/Altair-AA108-Camera-Drone-RC-Quadcopter-w-720p-HD-FPV-Camera-VR-Headless-Mode-Altitude-Hold-3-Skill-Modes-Great-for-Kids-Beginners-Easy-Fly-Indoor-Drone-2-Batteries.jpg",
+                        "buttons": [
+                            {
+                                "text": "View more",
+                                "postback": "https://mydeardrone.com/types/beginner/"
+                            }
+                        ]
+                    },
+                    "platform": "FACEBOOK"
+                },
+                {
+                    "card": {
+                        "title": "UDI U818A HD+",
+                        "subtitle": "BEST BEGINNER DRONE",
+                        "imageUri": "https://mydeardrone.com/wp-content/uploads/2018/03/Force1-UDI-U818A-Camera-Drone-for-Kids-HD-Drone-with-Camera-for-Beginners-720p-RC-Camera-Drones-w-360%C2%B0-Flips-Extra-Battery.jpg",
+                        "buttons": [
+                            {
+                                "text": "View more",
+                                "postback": "https://mydeardrone.com/types/beginner/"
+                            }
+                        ]
+                    },
+                    "platform": "FACEBOOK"
+                }
+            ];
+            responseJson.fulfillmentMessages = richResponses;
+            break;
+        default:
+            responseJson.fulfillmentText = 'Unknown action';
+    }
 
-    // Get the sender PSID
-    let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);
-
-});
+    response.json(responseJson);
+      
+      
+      
   });
 
 
